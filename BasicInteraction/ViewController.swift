e9ed30bccf6d6e864f9f70d9c540d27427a4b26b
Digices Dev <dev@digices.com>
2017-10-03 13:47:31 -0600

    Connect UI elements to code.

--- BasicInteraction/ViewController.swift
+++ BasicInteraction/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var nameField: UITextField!
+  
+  @IBOutlet weak var messageLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  @IBAction func didClickButton(_ sender: UIButton) {
+    if let name = self.nameField.text {
+      if name.characters.count > 0 {
+        self.messageLabel.text = "Hello \(name)!"
+      } else {
+        self.messageLabel.text = "Name cannot be blank :/"
+      }
+    }
+  }
+  
 }
 
